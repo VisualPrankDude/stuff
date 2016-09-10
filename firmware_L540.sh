7e116c5689fd1944763d30376994c5eccc2e418d
VisualPrankDude <VisualPrankDude@gmx.de>
2016-09-10 02:13:23 +0200

    format
    .

--- firmware_L540.sh
+++ firmware_L540.sh
@@ -11,5 +11,7 @@ exit 1
 fi
 
 
-cd /tmp && curl -# -4 -A Mozilla --connect-timeout 60 --interface enp0s25 \
--e "https://www.google.com/warez/isos/firmwareL540" -o "j4uj${n}wd.iso" "https://download.lenovo.com/ibmdl/pub/pc/pccbbs/mobiles/j4uj${n}wd.iso"
+cd /tmp && \
+curl -# -4 -A Mozilla --connect-timeout 60 --interface enp0s25 \
+-e "https://www.google.com/warez/isos/firmwareL540" -o "j4uj${n}wd.iso" \
+"https://download.lenovo.com/ibmdl/pub/pc/pccbbs/mobiles/j4uj${n}wd.iso"
