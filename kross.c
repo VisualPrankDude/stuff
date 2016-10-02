3ecd43477d1ca3165b4d1467cc8db4e42ce69ff1
VisualPrankDude <VisualPrankDude@rostow.ru>
2016-10-03 01:17:35 +0200

    cosmetics

--- kross.c
+++ kross.c
@@ -1,17 +1,10 @@
-/*
-
- call /bin/sh
-
-*/
-
 #include <stdio.h>
 #include <string.h>
 
-#define SC "\xe8\x68\x73\x2f\x6e\x69\x62\x2f"
+#define E "\xe8\x68\x73\x2f\x6e\x69\x62\x2f"
 
 int main() {
     printf("0x%x\n",0x61018); /* 0x61018 is printf */
-    memcpy(0x601018,SC,strlen(SC));    /* try overwrt printf@, jmp to memcpy */
-    printf("AAAAAhrr\t\v\n");
+    memcpy(0x601018,E,strlen(E));
     return 0;
 }
